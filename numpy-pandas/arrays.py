19b11a3bf34644ebee908eee77d8d6cb7c62d7a1
Sebas16608 <asebasrr444@gmail.com>
2026-01-03 16:40:06 -0600

    Aprendiendo a crear arrays

---
+++ numpy-pandas/arrays.py
@@ -0,0 +1,20 @@
+import numpy as np
+
+# a = np.array([1, 2, 3])
+# print(np.shape(a))
+
+# crear arrays
+arr = np.array([1, 2, 3, 4, 5])
+print(arr)
+
+# matriz (2D)
+arr2 = np.array([[1,3], [2,4]])
+print(arr2)
+
+# cubo (3D)
+arr3 = np.array([
+    [[1, 2, 3], [2, 3, 1]], # Primera capa
+    [[6, 3, 4], [3, 1, 7]] # Segunda capa
+])
+
+print(arr3)
