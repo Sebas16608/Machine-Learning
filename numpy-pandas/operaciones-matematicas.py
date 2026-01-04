37917bec41f6eada295e1d41814f6479cc02e48d
Sebas16608 <asebasrr444@gmail.com>
2026-01-03 22:18:15 -0600

    Aprendiendo operaciones matematicas con arrays

---
+++ numpy-pandas/operaciones-matematicas.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+x = np.array([1, 2, 3])
+y = np.array([3, 2, 5])
+
+# sumar
+print("sumar", x + y)
+print("resta", x - y)
+print("multiplicar", x*y)
+print("producto punto", x.dot(y))
